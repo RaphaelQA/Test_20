6099766b80873222c9b7ee566fb17a426c8ff0e0
yraf2 <y.raf2018@yandex.ru>
2023-02-06 22:15:05 +0300

    20

---
+++ demostration_solution/tests/test_service/test_genre.py
@@ -0,0 +1,60 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from dao.model.genre import Genre
+from dao.genre import GenreDAO
+from service.genre import GenreService
+from setup_db import db
+
+
+@pytest.fixture()
+def genre_dao():
+    genre_dao = GenreDAO(db.session)
+
+    comedy = Genre(id=1, name='comedy')
+    horror = Genre(id=2, name='horror')
+    drama = Genre(id=3, name='drama')
+
+    genre_dao.get_one = MagicMock(return_value=comedy)
+    genre_dao.get_all = MagicMock(return_value=[comedy, horror, drama])
+    genre_dao.create = MagicMock(return_value=Genre(id=3, name='thriller'))
+    genre_dao.delete = MagicMock()
+    genre_dao.update = MagicMock()
+
+    return genre_dao
+
+
+class TestGenreService:
+
+    @pytest.fixture(autouse=True)
+    def genre_service(self, genre_dao):
+        self.genre_service = GenreService(dao=genre_dao)
+
+    def test_get_one(self):
+        genre = self.genre_service.get_one(1)
+        assert genre is not None
+        assert genre.id == 1
+
+    def test_get_all(self):
+        genres = self.genre_service.get_all()
+        assert len(genres) == 3
+        assert len(genres) > 0
+
+    def test_create(self):
+        genre_data = {
+            'name': 'thriller'
+        }
+        genre = self.genre_service.create(genre_data)
+        assert genre.name == genre_data['name']
+
+    def test_delete(self):
+        genre = self.genre_service.delete(1)
+        assert genre is None
+
+    def test_update(self):
+        genre_data = {
+            'id': 3,
+            'name': 'action'
+        }
+        self.genre_service.update(genre_data)
---
+++ demostration_solution/tests/test_service/test_movies.py
@@ -0,0 +1,85 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from dao.model.movie import Movie
+from dao.movie import MovieDAO
+from service.movie import MovieService
+from setup_db import db
+
+
+@pytest.fixture()
+def movie_dao():
+    movie_dao = MovieDAO(db.session)
+
+    movie_1 = Movie(id=1,
+                    title='scary_movie',
+                    description='description1',
+                    trailer='trailer1',
+                    year=1991,
+                    rating=2,
+                    genre_id=1,
+                    director_id=1,
+                    )
+    movie_2 = Movie(id=1,
+                    title='scary_movie2',
+                    description='description2',
+                    trailer='trailer2',
+                    year=1992,
+                    rating=3,
+                    genre_id=2,
+                    director_id=2,
+                    )
+
+    movie_3 = Movie(id=3,
+                    title='scary_movie3',
+                    description='description3',
+                    trailer='trailer3',
+                    year=1993,
+                    rating=4,
+                    genre_id=3,
+                    director_id=3,
+                    )
+
+    movie_dao.get_one = MagicMock(return_value=movie_1)
+    movie_dao.get_all = MagicMock(return_value=[movie_1, movie_2, movie_3])
+    movie_dao.create = MagicMock(return_value=Movie(id=3, title='Interstellar'))
+    movie_dao.delete = MagicMock()
+    movie_dao.update = MagicMock()
+
+    return movie_dao
+
+
+class TestMovieService:
+
+    @pytest.fixture(autouse=True)
+    def movie_service(self, movie_dao):
+        self.movie_service = MovieService(dao=movie_dao)
+
+    def test_get_one(self):
+        movie = self.movie_service.get_one(1)
+        assert movie is not None
+        assert movie.id == 1
+
+    def test_get_all(self):
+        movies = self.movie_service.get_all()
+        assert len(movies) == 3
+        assert len(movies) > 0
+
+    def test_create(self):
+        movie_data = {
+            'title': 'Interstellar'
+        }
+        movie = self.movie_service.create(movie_data)
+        assert movie.title == movie_data['title']
+
+    def test_delete(self):
+        movie = self.movie_service.delete(1)
+        assert movie is None
+
+    def test_update(self):
+        movie_data = {
+            'id': 3,
+            'title': 'Start'
+        }
+        self.movie_service.update(movie_data)
